d3fb529e007ef2286f00000ef457bb85ae15994d
mr.Rhenium <mr.rhenium75614@gmail.com>
2021-12-08 21:32:13 +0530

    footer css

--- Files/footer.js
+++ Files/footer.js
@@ -1,4 +1,5 @@
 const list = document.querySelectorAll(".list");
+const indicator = document.querySelector(".indicator");
 
 function activeClass(e) {
   e.preventDefault();
@@ -6,6 +7,20 @@ function activeClass(e) {
     item.classList.remove("active");
     this.classList.add("active");
   });
+
+  if (/home/.test(this.classList.value)) {
+    indicator.style.transform = "translate(50%, -50%)";
+    console.log("hello");
+  } else if (/List/.test(this.classList.value)) {
+    indicator.style.transform = "translate(254%, -50%)";
+    console.log("list");
+  } else if (/profile/.test(this.classList.value)) {
+    indicator.style.transform = "translate(458%, -50%)";
+    console.log("profile");
+  } else if (/history/.test(this.classList.value)) {
+    indicator.style.transform = "translate(664%, -50%)";
+    console.log("history");
+  }
 }
 
 list.forEach((item) => {
